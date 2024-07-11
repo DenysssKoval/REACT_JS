757f7203f3c77453f364a2afcf2cad7a9b8da3f7
Denys Koval <denissskoval@gmail.com>
2024-07-11 09:53:18 +0200

    'lazy'

---
+++ lesson_01/my-react/src/app/components/myButton/MyButton.jsx
@@ -0,0 +1,10 @@
+
+import "./myButton.css";
+
+export default function MyButton({ type='button', onClick, name='undefined' }) {
+  return (
+    <button type={type} onClick={onClick} className="myButton">
+      {name}
+    </button>
+  );
+}
--- lesson_01/my-react/src/index.jsx
+++ lesson_01/my-react/src/index.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import "./index.css";
+import Lesson04 from "./app/lessons/lesson_04/Lesson04";
+// import Lesson01 from './lessons/lesson_01/Lesson01';
+// import Lesson02 from './lessons/lesson_02/Lesson02';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+    {/* <Lesson01 /> */}
+    {/* <Lesson02/> */}
+    {/* <MyForm/> */}
+    {/* <Counter /> */}
+    {/* <Homework04 /> */}
+    {/* <Lesson03 /> */}
+    <Lesson04 />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
